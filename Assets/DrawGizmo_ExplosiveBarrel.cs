19bd374080946feb170490d30f0099ab2c2fa973
nklskln <nikolas.klein@ue-germany.de>
2018-12-17 13:53:33 +0100

    Nikolas - added Gizmos

---
+++ Assets/DrawGizmo_ExplosiveBarrel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawGizmo_ExplosiveBarrel : MonoBehaviour
+{
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawIcon(transform.position, "gizmo_explosivebarrel2");
+    }
+}
---
+++ Assets/DrawGizmo_Trap.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawGizmo_Trap : MonoBehaviour
+{
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawIcon(transform.position, "gizmo_trap2");
+    }
+}
