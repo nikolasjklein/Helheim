42c5932821d6071ace33b053dad83ebd1565b3c1
nklskln <nikolas.klein@ue-germany.de>
2018-12-22 12:16:30 +0100

    Nikolas - added working Health Pickup

---
+++ Assets/PickUp_Health.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp_Health : MonoBehaviour
+{
+    public PlayerController playerControllerIntegration;
+    public int HealthToAdd = 15;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerControllerIntegration.playerLife += HealthToAdd;
+
+            if (playerControllerIntegration.playerLife > 100)
+            {
+                playerControllerIntegration.playerLife = 100;
+            }
+            Destroy(gameObject);
+        }
+    }
+}
