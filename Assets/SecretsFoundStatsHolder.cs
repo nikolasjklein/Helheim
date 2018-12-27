3f4229fc48c0d88f9cb80724027889c7961bfdf6
nklskln <nikolas.klein@ue-germany.de>
2018-12-27 15:35:29 +0100

    Nikolas - added Secret Finding System

---
+++ Assets/SecretsFoundStatsHolder.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SecretsFoundStatsHolder : MonoBehaviour
+{
+    public int SecretsMax;
+    public int SecretsCur;
+    public Text SecretsUpdateText;
+    public float delay;
+
+    public void Start()
+    {
+        SecretsMax = SecretsMax;
+        SecretsCur = 0;
+
+        SecretsUpdateText.gameObject.SetActive(false);
+    }
+
+    public void Update()
+    {
+        
+    }
+
+    public void FoundNewSecret()
+    {
+        SecretsCur = SecretsCur + 1;
+        SecretsUpdateText.text = "Secret Found\n" + SecretsCur + " / " + SecretsMax;
+        SecretsUpdateText.gameObject.SetActive(true);
+        StartCoroutine(DisplaySecretText(SecretsUpdateText.gameObject, delay));
+    }
+
+    public void HideSecretText ()
+    {
+        SecretsUpdateText.gameObject.SetActive(false);
+    }
+
+    private IEnumerator DisplaySecretText(GameObject text, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        HideSecretText();
+    }
+}
---
+++ Assets/TriggerSecret.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSecret : MonoBehaviour
+{
+    public SecretsFoundStatsHolder secretFoundStatsHolder;
+    public bool foundSecret;
+
+	void Start ()
+    {
+        foundSecret = false;
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    public void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Player is in Trigger Area");
+
+        if (other.tag == "Player")
+        {
+            Debug.Log("Player is tagged Player");
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (!foundSecret)
+                {
+                    Debug.Log("Pressed Space");
+                    secretFoundStatsHolder.FoundNewSecret();
+                    foundSecret = true;
+                }
+                
+                else
+                {
+                    Debug.Log("Already found Secret!");
+                }
+            }
+        }
+    }
+}
