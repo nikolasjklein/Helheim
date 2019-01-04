53a619f86c20274b95e98371d599b21fffd6bd43
nklskln <nikolas.klein@ue-germany.de>
2019-01-04 15:16:49 +0100

    Nikolas - added prototype launch menu

---
+++ Assets/StoryButton.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StoryButton : MonoBehaviour
+{
+    public Button ContinueButton;
+    public Button OptionsButton;
+    public Button StorySettingsButton;
+    public Button NewGameButton;
+
+    public bool isToggled = false;
+
+    void Start ()
+    {
+        isToggled = false;
+
+        StorySettingsButton.gameObject.SetActive(false);
+        NewGameButton.gameObject.SetActive(false);
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    public void ClickContinueButton()
+    {
+        SceneManager.LoadScene("LaunchScene", LoadSceneMode.Single);
+        Debug.Log("Continue Clicked");
+    }
+
+    public void ClickOptionsButton()
+    {
+        if (isToggled)
+        {
+            isToggled = false;
+
+            StorySettingsButton.gameObject.SetActive(false);
+            NewGameButton.gameObject.SetActive(false);
+        }
+
+        else if (!isToggled)
+        {
+            isToggled = true;
+
+            StorySettingsButton.gameObject.SetActive(true);
+            NewGameButton.gameObject.SetActive(true);
+        }
+    }
+
+    public void ClickStorySettingsButton()
+    {
+        Debug.Log("Story Settings Clicked");
+    }
+
+    public void ClickNewGameButton()
+    {
+        Debug.Log("New Game Clicked");
+    }
+}
