a131a5081f0c74cf4d9c298b1f3d124721a4d297
nklskln <nikolas.klein@ue-germany.de>
2019-01-31 23:03:27 +0100

    Nikolas - added Location Indicator Text Script

---
+++ Assets/Scripts/TriggerLocationUpdate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriggerLocationUpdate : MonoBehaviour
+{
+    public Animator locationUpdateAnimator;
+    public Text locationUpdateText;
+    public string locationName;
+    public float delay = 5f;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            locationUpdateText.text = locationName;
+            locationUpdateAnimator.SetBool("isTriggered", true);
+            StartCoroutine(DisableLocationText(delay));
+        }
+    }
+
+    private IEnumerator DisableLocationText(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        //run function
+        DisableLocationUpdateAnim();
+    }
+
+    public void DisableLocationUpdateAnim()
+    {
+        locationUpdateAnimator.SetBool("isTriggered", false);
+    }
+}
