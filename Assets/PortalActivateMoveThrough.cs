89eda6f46b683c7b2c77dc1d54cb026df02c0ae6
nklskln <nikolas.klein@ue-germany.de>
2019-03-10 23:50:04 +0100

    Nikolas - added Portal Functionality

---
+++ Assets/PortalActivateMoveThrough.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalActivateMoveThrough : MonoBehaviour
+{
+    public GameObject PortalEntryPoint;
+
+    public void Start()
+    {
+        PortalEntryPoint.gameObject.SetActive(false);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PortalEntryPoint.gameObject.SetActive(true);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PortalEntryPoint.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/PortalMoveThrough.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalMoveThrough : MonoBehaviour
+{
+    public Transform PortalExitPoint;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.position = PortalExitPoint.transform.position;
+            other.transform.rotation = PortalExitPoint.transform.rotation;
+        }
+    }
+}
