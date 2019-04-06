96f8f977d379e052d255162750a3740460af3bea
nklskln <nikolas.klein@ue-germany.de>
2019-04-06 15:02:20 +0200

    Nikolas - removed some comments

--- Assets/Scripts/Inventory/InventoryUI.cs
+++ Assets/Scripts/Inventory/InventoryUI.cs
@@ -424,107 +424,107 @@ public class InventoryUI : MonoBehaviour
 
     public void UnEquipArmor()
     {
-        if (/*name == "LeatherArmor" && */player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "LeatherArmor")
+        if (player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "LeatherArmor")
         {
             player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped = "Unarmored";
         }
 
-        else if (/*name == "BoarHideArmor" && */player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "BoarHideArmor")
+        else if (player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "BoarHideArmor")
         {
             player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped = "Unarmored";
         }
 
-        else if (/*name == "WolfSkinArmor" && */player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "WolfSkinArmor")
+        else if (player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "WolfSkinArmor")
         {
             player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped = "Unarmored";
         }
 
-        else if (/*name == "ChainmailArmor" && */player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "ChainmailArmor")
+        else if (player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "ChainmailArmor")
         {
             player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped = "Unarmored";
         }
 
-        else if (/*name == "AncientArmor" && */player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "AncientArmor")
+        else if (player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "AncientArmor")
         {
             player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped = "Unarmored";
         }
 
-        else if (/*name == "RunicArmor" && */player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "RunicArmor")
+        else if (player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "RunicArmor")
         {
             player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped = "Unarmored";
         }
 
-        else if (/*name == "SlaughterersArmor" && */player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "SlaughterersArmor")
+        else if (player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "SlaughterersArmor")
         {
             player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped = "Unarmored";
         }
 
-        else if (/*name == "MithrilArmor" && */player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "MithrilArmor")
+        else if (player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "MithrilArmor")
         {
             player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped = "Unarmored";
         }
 
-        else if (/*name == "ValkyrieInfusedArmor" && */player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "ValkyrieInfusedArmor")
+        else if (player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "ValkyrieInfusedArmor")
         {
             player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped = "Unarmored";
         }
 
-        else if (/*name == "MuspelheimArmor" && */player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "MuspelheimArmor")
+        else if (player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "MuspelheimArmor")
         {
             player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped = "Unarmored";
         }
 
-        else if (/*name == "AlfheimArmor" && */player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "AlfheimArmor")
+        else if (player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "AlfheimArmor")
         {
             player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped = "Unarmored";
         }
 
-        else if (/*name == "NiflheimArmor" && */player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "NiflheimArmor")
+        else if (player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "NiflheimArmor")
         {
             player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped = "Unarmored";
         }
 
-        else if (/*name == "MidgardArmor" && */player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "MidgardArmor")
+        else if (player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "MidgardArmor")
         {
             player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped = "Unarmored";
         }
 
-        else if (/*name == "AsgardArmor" && */player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "AsgardArmor")
+        else if (player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "AsgardArmor")
         {
             player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped = "Unarmored";
         }
 
-        else if (/*name == "JotunheimArmor" && */player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "JotunheimArmor")
+        else if (player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "JotunheimArmor")
         {
             player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped = "Unarmored";
         }
 
-        else if (/*name == "VanaheimArmor" && */player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "VanaheimArmor")
+        else if (player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "VanaheimArmor")
         {
             player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped = "Unarmored";
         }
 
-        else if (/*name == "SvartalfheimArmor" && */player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "SvartalfheimArmor")
+        else if (player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "SvartalfheimArmor")
         {
             player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped = "Unarmored";
         }
 
-            else if (/*name == "HelheimArmor" && */player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "HelheimArmor")
+            else if (player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "HelheimArmor")
         {
             player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped = "Unarmored";
         }
 
-        else if (/*name == "DoomGuyArmor" && */player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "DoomGuyArmor")
+        else if (player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "DoomGuyArmor")
         {
             player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped = "Unarmored";
         }
 
-        else if (/*name == "Stormtrooperarmor" && */player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "Stormtrooperarmor")
+        else if (player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "Stormtrooperarmor")
         {
             player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped = "Unarmored";
         }
 
-        else if (/*name == "GalaxyGlove" && */player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "GalaxyGlove")
+        else if (player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped == "GalaxyGlove")
         {
             player.GetComponentInChildren<ArmorManager>().whichArmorIsEquipped = "Unarmored";
         }
